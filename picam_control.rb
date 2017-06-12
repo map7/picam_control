87ec32e734928c08d53c8fc1ecc951b8287c230e
Michael Pope <map7777@gmail.com>
2017-06-12 18:15:02 +1000

    Add flash messages and better control

--- picam_control.rb
+++ picam_control.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
-require 'byebug'
+require 'sinatra/flash'
+
+enable :sessions
 
 BASE_DIR = "#{ENV['HOME']}/picam/hooks"
 
@@ -14,12 +16,34 @@ get '/' do
   erb :index
 end
 
+get '/restart_nginx' do
+  `sudo service nginx restart`
+  flash[:info] = "Nginx restarted"
+  redirect "/"
+end
+
+get '/start_picam' do
+  width = params[:width].to_i
+  height = params[:height].to_i
+  fps=params[:fps].to_i
+
+  if File.exist?("#{ENV['HOME']}/picam")
+    `cd #{ENV['HOME']}/picam;./picam --tcpout tcp://127.0.0.1:8181 --alsadev hw:1,0 -w #{width} -h #{height} --fps #{fps}`
+    flash[:info] = "PICAM started #{width}x#{height}@#{fps}"
+  else
+    flash[:warning] = "PICAM doesn't exist!"
+  end
+  redirect "/"
+end
+
 get '/start' do 
+  flash[:info] = "Recording started"
   `touch #{BASE_DIR}/start_record`
   redirect "/"
 end
 
 get '/stop' do
+  flash[:info] = "Recording stopped"
   `touch #{BASE_DIR}/stop_record`
   redirect "/"
 end
